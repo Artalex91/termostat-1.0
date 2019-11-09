224e2b16524242ee31d604af4701b7e2c5bcc0a4
Artalex91 <mobart.91@mail.ru>
2019-11-09 08:46:24 +0300

    test lm35

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+float tempC;
+int reading;
+int tempPin = 0;
+
+void setup()
+{
+analogReference(INTERNAL);
+Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+reading = analogRead(tempPin);
+tempC = reading / 9.31;
+Serial.println(tempC);
+delay(1000);
 }
